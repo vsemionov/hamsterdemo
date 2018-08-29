90275a7a0a18f12e3b9ca026fcf213ed2ee178e0
Victor Semionov <vsemionov@gmail.com>
2018-08-30 01:56:25 +0300

    customize admin name

--- api/admin.py
+++ api/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Notebook, Note
 
 
+admin.site.site_header = 'HamsterDemo administration'
+admin.site.site_title = 'HamsterDemo site admin'
+
+
 class NotebookAdmin(admin.ModelAdmin):
     pass
 
