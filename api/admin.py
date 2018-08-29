0cc87ffb0e4573ece0b9526320b8fc70d8b13753
Victor Semionov <vsemionov@gmail.com>
2018-08-30 00:45:53 +0300

    defined models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Notebook, Note
+
+
+class NotebookAdmin(admin.ModelAdmin):
+    pass
+
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Notebook, admin.ModelAdmin)
+admin.site.register(Note, NoteAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+MAX_NAME_SIZE = 128
+MAX_TEXT_SIZE = 32*1024
+
+
+class Notebook(models.Model):
+    name = models.CharField(max_length=MAX_NAME_SIZE)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    notebook = models.ForeignKey(Notebook, models.PROTECT)
+
+    title = models.CharField(max_length=MAX_NAME_SIZE)
+    text = models.TextField(max_length=MAX_TEXT_SIZE)
+
+    def __str__(self):
+        return self.title
