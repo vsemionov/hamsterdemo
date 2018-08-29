c8a48f3ebbba29b4a51640125fa0f11aee19f70c
Victor Semionov <vsemionov@gmail.com>
2018-08-30 01:56:55 +0300

    use postgres on heroku

--- hamsterdemo/settings.py
+++ hamsterdemo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import dj_database_url
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,6 +84,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'].update(dj_database_url.config(conn_max_age=1800))
 
 
 # Password validation
